ac28b862c74615c90f84ad8c7d6f6888124e8f82
Amit lavania <amit.lavania_cs20@gla.ac.in>
2022-05-29 23:24:59 +0530

    Delete gitupload directory

--- gitupload/downward_full_pyramid.py
+++
@@ -1,9 +0,0 @@
-rows = int(input('Enter the number of rows:  '))
-col = 2*rows-2
-for i in range(rows,-1,-1):
-    for j in range(col,0,-1):
-        print(end=" ")
-    col +=1
-    for j in range(0,i+1):
-        print("*",end=" ")
-    print("")
